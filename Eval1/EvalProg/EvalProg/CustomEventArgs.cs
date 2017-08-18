c919eaf75ef7d0cab0b1e4a37eae4c0e1eceb2ca
Kaushik Nath <kn.nath.kaushik@gmail.com>
2017-08-18 11:11:56 +0530

    Eval Program first

---
+++ Eval1/EvalProg/EvalProg/CustomEventArgs.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace EvalProg
+{
+    public class CustomEventArgs : EventArgs
+    {
+        private int age;
+        public CustomEventArgs(int age)
+        {
+            this.age = age;
+        }
+        public int getOldAge()
+        {
+            return age;
+        }
+    }
+}
---
+++ Eval1/EvalProg/EvalProg/Customer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace EvalProg
+{
+    public delegate void CustomEventhandler(object source, CustomEventArgs e);
+    class Customer
+    {
+        public event CustomEventhandler invalidAgeHandler;
+        static string age;
+        public string Age
+        {
+            get
+            {
+                return age;
+            }
+            set
+            {
+                if (Convert.ToInt32(value) < 0 || Convert.ToInt32(value) > 200)
+                {
+                    onInValidAge();
+                }
+                else age = value;
+            }
+        }
+
+        private void onInValidAge()
+        {
+            CustomEventhandler handler = invalidAgeHandler;
+            if (handler != null)
+            {
+                handler(this, new CustomEventArgs(Convert.ToInt32(age)));
+            }
+
+        }
+    }
+}
