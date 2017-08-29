3ee7553bb6880abcb04cb36c077a5b84bf3a1349
KaushikNathMIT <kauanath@gmail.com>
2017-08-29 07:44:12 +0530

    Assignment 2 question 2 added

---
+++ Assignment1/Question2/Question2/Bus.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question2 {
+	public delegate void CustomEventHandler(object source, EventArgs e);
+	class Bus {
+		float speed, fuel;
+		int numberOfPassengers;
+		public float Speed {
+			get {
+				return speed;
+			}
+			set {
+				speed = value;
+				onSpeedChanged(speed);
+			}
+		}
+
+		private void onSpeedChanged(float speed) {
+			if (speed > 70) {
+				speedHandler(this, new EventArgs());
+			}
+		}
+
+		public float Fuel {
+			get {
+				return fuel;
+			}
+			set {
+				fuel = value;
+				onFuelChanged(fuel);
+			}
+		}
+
+		private void onFuelChanged(float fuel) {
+			if (fuel < 5) fuelHandler(this, new EventArgs());
+		}
+
+		public int NumberOfPassengers {
+			get {
+				return numberOfPassengers;
+			}
+			set {
+				numberOfPassengers = value;
+				onNumberOfPassengersChanged(numberOfPassengers);
+			}
+		}
+
+		private void onNumberOfPassengersChanged(int numberOfPassengers) {
+			if (numberOfPassengers > 60) nopHandler(this, new EventArgs());
+		}
+
+		public event CustomEventHandler speedHandler, nopHandler, fuelHandler;
+
+	}
+}
---
+++ Assignment1/Question2/Question2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question2 {
+	class Program {
+		static void Main(string[] args) {
+			Bus bus = new Bus();
+			bus.speedHandler += new CustomEventHandler(onChangedSpeed);
+			bus.fuelHandler += new CustomEventHandler(onChangedFuel);
+			bus.nopHandler += new CustomEventHandler(onChangedNOP);
+			bus.Speed = 10;
+			bus.Speed = 100;
+			bus.Speed = 20;
+			bus.Speed = 90;
+			bus.Fuel = 20;
+			bus.Fuel = 4;
+			bus.NumberOfPassengers = 10;
+			bus.NumberOfPassengers = 100;
+			Console.ReadLine();
+		}
+
+		private static void onChangedNOP(object source, EventArgs e) {
+			Console.WriteLine("Check Passengers");
+		}
+
+		private static void onChangedFuel(object source, EventArgs e) {
+			Console.WriteLine("Check Fuel");
+		}
+
+		private static void onChangedSpeed(object source, EventArgs e) {
+			Console.WriteLine("Check Speed");
+		}
+	}
+}
