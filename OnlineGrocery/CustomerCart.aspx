94eaa040e88c228de7bab0a6ba7219204c27fc01
KaushikNathMIT <kauanath@gmail.com>
2017-10-25 02:35:02 +0530

    Customer cart done

--- OnlineGrocery/CustomerCart.aspx
+++ OnlineGrocery/CustomerCart.aspx
@@ -9,6 +9,20 @@
 <body>
     <form id="form1" runat="server">
         <div>
+			<asp:Label ID="orderid" runat="server" />
+			<br />
+			<asp:SqlDataSource ID="ds2" runat="server" SelectCommand="select ProductName from Product" ConnectionString="<%$ConnectionStrings:db%>" />
+			<asp:DropDownList ID="dpd_list_prod" runat="server" DataSourceID="ds2" DataTextField="productname" /> 
+			<asp:TextBox ID="tb_quantity" runat="server" />
+			<asp:Button Text="Update" ID="btn_add" OnClick="btn_add_Click" runat="server" />
+		</div>
+		<div>
+            <asp:SqlDataSource ID="ds1" runat="server" SelectCommand="select * from [Order] where orderid = @orderid" ConnectionString="<%$ConnectionStrings:db%>" >
+				<SelectParameters>
+					<asp:ControlParameter Name="orderid" ControlID="orderid" Type="String" />
+				</SelectParameters>	
+			</asp:SqlDataSource>
+            <asp:GridView DataSourceID="ds1" runat="server" ID="gv_cart"/>
         </div>
     </form>
 </body>
--- OnlineGrocery/CustomerCart.aspx.cs
+++ OnlineGrocery/CustomerCart.aspx.cs
@@ -1,14 +1,65 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
+using System.Web.Configuration;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Diagnostics;
 
 public partial class CustomerCart : System.Web.UI.Page
 {
-    protected void Page_Load(object sender, EventArgs e)
-    {
+	private string orderID=null;
+	private string productid;
+	protected void Page_PreLoad(object sender, EventArgs e)
+	{
+		/*
+		if (orderID == null)
+		{
+			orderID = GenerateNewOrderID();
+		}
+		orderid.Text = orderID;
+		this.DataBind();
+		*/
+	}
+	protected void Page_Load(object sender, EventArgs e)
+	{
+		orderID = "Order0";
+		orderid.Text = orderID;
+		//this.DataBind();
+	}
 
-    }
+	private string GenerateNewOrderID()
+	{
+		SqlConnection connection = new SqlConnection();
+		connection.ConnectionString = WebConfigurationManager.ConnectionStrings["db"].ConnectionString;
+		SqlCommand command = new SqlCommand("select orderid from [Order]", connection);
+		connection.Open();
+		using (connection)
+		{
+			SqlDataReader sqlDataReader = command.ExecuteReader();
+			int count = 0;
+			while (sqlDataReader.Read())
+			{
+				count++;
+			}
+
+			return "Order" + count;
+		}
+	}
+
+	protected void btn_add_Click(object sender, EventArgs e)
+	{
+		SqlConnection connection = new SqlConnection();
+		connection.ConnectionString = WebConfigurationManager.ConnectionStrings["db"].ConnectionString;
+		Debug.WriteLine(dpd_list_prod.SelectedValue);
+		SqlCommand command = new SqlCommand("insert into [Order] values ('" + orderID + "','" + dpd_list_prod.SelectedValue + "','" + tb_quantity.Text + "')", connection);
+		connection.Open();
+		using (connection)
+		{
+			command.ExecuteNonQuery();
+		}
+		this.DataBind();
+	}
 }
