7e1b5a1e071683b9d2aa31384396db94b288330e
KaushikNathMIT <kauanath@gmail.com>
2017-10-25 21:15:26 +0530

    Order status for vendor

--- OnlineGrocery/CustomerHome.aspx.cs
+++ OnlineGrocery/CustomerHome.aspx.cs
@@ -9,9 +9,24 @@ using System.Web.UI.WebControls;
 
 public partial class CustomerHome : System.Web.UI.Page
 {
+	private string orderID;
 	protected void Page_Load(object sender, EventArgs e)
 	{
-		Response.Redirect("CustomerCart.aspx?orderid="+GenerateNewOrderID());
+		orderID = GenerateNewOrderID();
+		updateOrderStatusTable();
+		Response.Redirect("CustomerCart.aspx?orderid=" + orderID);
+	}
+
+	private void updateOrderStatusTable()
+	{
+		SqlConnection connection = new SqlConnection();
+		connection.ConnectionString = WebConfigurationManager.ConnectionStrings["db"].ConnectionString;
+		SqlCommand command = new SqlCommand("insert into OrderStatus values('" + orderID + "','PENDING')", connection);
+		connection.Open();
+		using (connection)
+		{
+			command.ExecuteNonQuery();
+		}
 	}
 
 	private string GenerateNewOrderID()
---
+++ OnlineGrocery/VendorPending.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="VendorPending.aspx.cs" Inherits="VendorPending" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+			s
+			<asp:Button Text="Fast forward 1/4th day"
+        </div>
+    </form>
+</body>
+</html>
