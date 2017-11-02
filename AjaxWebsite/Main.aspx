df711f9d34006504668d28f1b025b7d1dd6232a2
KaushikNathMIT <kauanath@gmail.com>
2017-11-02 10:01:47 +0530

    Ajax website complete

---
+++ AjaxWebsite/Main.aspx
@@ -0,0 +1,42 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Main.aspx.cs" Inherits="Main" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+	<title></title>
+</head>
+<body>
+	<form id="form1" runat="server">
+		<div>
+			<asp:ScriptManager ID="sm" runat="server" />
+			<br />
+			<br />
+			<asp:UpdatePanel ID="UpdatePanel1" runat="server">
+				<ContentTemplate>
+					<asp:Panel ID="panel_counter" GroupingText="Customers" runat="server"/>
+					<br />
+					Number of customers
+					<asp:Label ID="label_num" runat="server" />
+					<br />
+					<asp:Label ID="Label2" runat="server" Text="Enter your name"/>
+					<asp:TextBox ID="tb_name" runat="server" />
+					<br />
+					<asp:RequiredFieldValidator ControlToValidate="tb_name" runat="server" ErrorMessage="Name can't be empty"/>
+					<br />
+					<asp:DropDownList ID="dpd_list" runat="server" />
+					<br />
+					Select your gender
+					<asp:RadioButtonList ID="rb_list" runat="server" />
+					<br />
+					<asp:Button ID="button_submit" Text="Add user" OnClick="button_submit_Click" runat="server" />
+					<br />
+					<asp:SqlDataSource ID="ds" runat="server" SelectCommand="select * from Customer" ConnectionString="<%$ConnectionStrings:db%>"/>
+					<asp:GridView DataSourceID="ds" runat="server" />
+					<br />
+				</ContentTemplate>
+			</asp:UpdatePanel>
+		</div>
+	</form>
+</body>
+</html>
---
+++ AjaxWebsite/Main.aspx.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Main : System.Web.UI.Page
+{
+	protected void Page_Load(object sender, EventArgs e)
+	{
+		if (!IsPostBack)
+		{
+			Session["custid"] = -1;
+			HttpCookie httpCookie = new HttpCookie("pref");
+			httpCookie["num"] = "-1";
+			Response.Cookies.Add(httpCookie);
+			rb_list.Items.Add("Male");
+			rb_list.Items.Add("Female");
+			dpd_list.Items.Add("Select preferred payment method");
+			dpd_list.Items.Add("Online Payment");
+			dpd_list.Items.Add("Cash On Delivery");
+		}
+	}
+
+	protected void button_submit_Click(object sender, EventArgs e)
+	{
+		this.Validate();
+		if (IsValid)
+		{
+			SqlConnection connection = new SqlConnection();
+			connection.ConnectionString = WebConfigurationManager.ConnectionStrings["db"].ConnectionString;
+			string s = "insert into Customer values(" + getNewCustomerID() + ", '" + tb_name.Text + "', '" + rb_list.SelectedValue + "', '" + dpd_list.SelectedValue + "')";
+			SqlCommand command = new SqlCommand(s, connection);
+			connection.Open();
+			using (connection)
+			{
+				command.ExecuteNonQuery();
+			}
+			HttpCookie httpCookie = Request.Cookies["pref"];
+			label_num.Text = httpCookie["num"];
+			this.DataBind();
+		}
+	}
+
+	private int getNewCustomerID()
+	{
+		int currNum = int.Parse(Session["custid"].ToString());
+		currNum++;
+		HttpCookie httpCookie = Request.Cookies["pref"];
+		httpCookie["num"] = currNum.ToString();
+		Session["custid"] = currNum;
+		return currNum;
+	}
+}
