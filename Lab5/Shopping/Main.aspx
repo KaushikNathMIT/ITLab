91eb0a2a6f33aede2caf9ae55b095b1ca68981d0
KaushikNathMIT <kauanath@gmail.com>
2017-11-08 23:03:01 +0530

    Additional lab 5 q1

---
+++ Lab5/Shopping/Main.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Main.aspx.cs" Inherits="Main" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+			<asp:DropDownList ID="dpd_prod" runat="server" />
+			<br />
+			Set quantity
+			<asp:TextBox ID="tb_quant" runat="server" />
+			<br />
+			<asp:Button ID="add" Text="Add" runat="server" OnClick="add_Click" />
+			<br />
+			<asp:Label ID="label_cart_details" runat="server"/>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Lab5/Shopping/Main.aspx.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Main : System.Web.UI.Page
+{
+	protected void Page_Load(object sender, EventArgs e)
+	{
+		HttpCookie httpCookie;
+		if (!IsPostBack)
+		{
+			if (Request.Cookies["pref"] == null)
+			{
+				httpCookie = new HttpCookie("pref");
+				httpCookie["numLaptop"] = "0";
+				httpCookie["numMobile"] = "0";
+				httpCookie.Expires = DateTime.Now.AddYears(2);
+				Response.Cookies.Add(httpCookie);
+			}
+			else httpCookie = Request.Cookies["pref"];
+			dpd_prod.Items.Add("Laptop");
+			dpd_prod.Items.Add("Mobile");
+		}
+		else httpCookie = Request.Cookies["pref"];
+		label_cart_details.Text = "Laptops : " + httpCookie["numLaptop"] + "\t Mobiles : " + httpCookie["numMobile"];
+	}
+
+	protected void add_Click(object sender, EventArgs e)
+	{
+		HttpCookie httpCookie = Request.Cookies["pref"];
+		int quantity = int.Parse(tb_quant.Text);
+		if (dpd_prod.SelectedItem.ToString().Equals("Laptop"))
+		{
+			httpCookie["numLaptop"] = (int.Parse(httpCookie["numLaptop"]) + quantity).ToString();
+		}
+		else if (dpd_prod.SelectedItem.ToString().Equals("Mobile"))
+		{
+			httpCookie["numMobile"] = (int.Parse(httpCookie["numMobile"]) + quantity).ToString();
+		}
+		httpCookie.Expires = DateTime.Now.AddYears(2);
+		Response.Cookies.Add(httpCookie);
+		label_cart_details.Text = "Laptops : " + httpCookie["numLaptop"] + "\t Mobiles : " + httpCookie["numMobile"];
+	}
+}
