ddca100dffc2243268500f935b755cf6d5bb6de2
KaushikNathMIT <kauanath@gmail.com>
2017-11-08 21:43:58 +0530

    lab 4 q 1

---
+++ Lab4/Magazine/Main.aspx
@@ -0,0 +1,34 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Main.aspx.cs" Inherits="Main" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+	<title></title>
+</head>
+<body>
+	<form id="form1" runat="server">
+		<div>
+			Choose image
+			<asp:DropDownList ID="dpd_image" runat="server" OnSelectedIndexChanged="dpd_image_SelectedIndexChanged" />
+			<br />
+			Choose background colour
+			<asp:DropDownList ID="dpd_bg_color" runat="server" OnSelectedIndexChanged="dpd_bg_color_SelectedIndexChanged" />
+			<br />
+			Choose font size : 
+			<asp:TextBox ID="tb_font_size" runat="server" />
+			<br />
+			Choose font colour :
+			<asp:DropDownList ID="dpd_font_color" runat="server" />
+			<br />
+			<asp:Button ID="apply" Text="Apply Theme" runat="server" OnClick="apply_Click" />
+		</div>
+		<div>
+			<br />
+			<asp:Image ID="CoverImage" runat="server" Width="50%" BorderWidth="20px"/>
+			<br />
+			<asp:Label ID="CoverBody" runat="server" />
+		</div>
+	</form>
+</body>
+</html>
---
+++ Lab4/Magazine/Main.aspx.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Main : System.Web.UI.Page
+{
+	protected void Page_Load(object sender, EventArgs e)
+	{
+		if(!IsPostBack)
+		{
+			foreach (KnownColor r in Enum.GetValues(typeof(KnownColor)))
+			{
+				ListItem item = new ListItem(Enum.GetName(typeof(KnownColor), r), r.ToString());
+				dpd_bg_color.Items.Add(item);
+				dpd_font_color.Items.Add(item);
+			}
+
+			DirectoryInfo diFiles = new DirectoryInfo(Server.MapPath("~/bgImages/"));
+			dpd_image.DataSource = diFiles.GetFiles("*.jpg");
+			dpd_image.DataBind();
+		}
+	}
+
+	protected void dpd_image_SelectedIndexChanged(object sender, EventArgs e)
+	{
+
+	}
+
+	protected void dpd_bg_color_SelectedIndexChanged(object sender, EventArgs e)
+	{
+
+	}
+
+	protected void apply_Click(object sender, EventArgs e)
+	{
+		//preview.Style[HtmlTextWriterStyle.BackgroundColor] = backgroundList.SelectedItem.Value;
+		CoverImage.ImageUrl = "~/bgImages/" + dpd_image.SelectedItem.Text; ;
+		CoverImage.Style[HtmlTextWriterStyle.BorderColor] = dpd_bg_color.SelectedItem.Value;
+		CoverBody.Style[HtmlTextWriterStyle.Color] = dpd_font_color.SelectedItem.Value;
+		CoverBody.Text = "Hello";
+		int fontSize;
+		if (int.TryParse(tb_font_size.Text, out fontSize))
+		{
+			CoverBody.Font.Size = fontSize;
+		}
+
+	}
+}
